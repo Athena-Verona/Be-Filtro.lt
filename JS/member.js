6231921a12df9560e0130363dd4b81f5788d7563
Roooooooooooookas <rokas.kartenis@gmail.com>
2022-12-05 01:43:20 +0200

    Comment section window (update)
    Comment section pop-up window now works on different screen sizes;
    Added some misc stuff;

--- JS/member.js
+++ JS/member.js
@@ -1,7 +1,7 @@
 // Automatically increases comment section text input height
 function textAreaAdjust(element) {
     element.style.height = "1px";
-    element.style.height = (25 + element.scrollHeight) + "px";
+    element.style.height = (30 + element.scrollHeight) + "px";
 }
 
 // Opens and closes comment section
@@ -10,12 +10,53 @@ function openClose_section() {
     if(open_status) {
         $(document).ready(function(){
             $(".comment-section").css("display", "unset");
+            $("body").css("overflow", "hidden");
             open_status = false;
         });
     } if(!open_status) {
         $(document).ready(function(){
             $(".comment-section").css("display", "none");
+            $("body").css("overflow", "scroll");
             open_status = true;
+        });
+    }
+}
+
+// Stuff cluttered into one jQuery block
+$(document).ready(function(){
+    $(".txta").focusin(function(){
+        $(".txta").keyup(function(){
+            document.getElementById('symbol-count').innerHTML = 255 - this.value.length;
+            if($(".txta").val().length <= 3) {
+                $("#post-button").css("filter", "grayscale(100%)");
+                $("#alert-symbol").css("visibility", "visible");
+            } else {
+                $("#post-button").css("filter", "grayscale(0)");
+                $("#alert-symbol").css("visibility", "hidden");
+            }
+        });
+    });
+    $(".txta").focusout(function(){
+        $("#alert-symbol").css("visibility", "hidden");
+    });
+});
+
+// Hides and unhides thread's text window
+var hide_status = true;
+function hideClose() {
+    if(hide_status) {
+        $(document).ready(function(){
+            $("#hide-text").css("transform", "rotate(-180deg)");
+            $(".comments-row").css("height", "620px");
+            $("#hide-this").slideToggle();
+            hide_status = false;
+        });
+    } if(!hide_status) {
+        $(document).ready(function(){
+            $("#hide-text").css("transform", "rotate(0deg)");
+            $(".comments-row").css("height", "520px");
+            $("#hide-this").slideToggle();
+            hide_status = true;
         });
     }
 }
