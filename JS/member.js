6d9e72c5b97999e3d8b07e8b93d3006c832d0044
Roooooooooooookas <rokas.kartenis@gmail.com>
2022-12-06 15:47:21 +0200

    Open discussion window (member information page)
    Added a pop-up window for opening a discussion;
    Moved comment-section & open-discussion styles into separate files because member information page style was getting crammed;

--- JS/member.js
+++ JS/member.js
@@ -1,21 +1,39 @@
 // Automatically increases comment section text input height
 function textAreaAdjust(element) {
     element.style.height = "1px";
-    element.style.height = (30 + element.scrollHeight) + "px";
+    element.style.height = (25 + element.scrollHeight) + "px";
 }
 
-// Opens and closes comment section
-var open_status = true;
-function openClose_section() {
+// Opens and closes comment section / open-discussion window
+function openClose_comments(open_status) {
     if(open_status) {
         $(document).ready(function(){
-            $(".comment-section").css("display", "unset");
+            $(".comment-section-background").fadeTo(200, 1);
+            $(".comment-section").fadeTo(200, 1);
             $("body").css("overflow", "hidden");
             open_status = false;
         });
     } if(!open_status) {
         $(document).ready(function(){
-            $(".comment-section").css("display", "none");
+            $(".comment-section-background").fadeOut(200);
+            $(".comment-section").fadeOut(200);
+            $("body").css("overflow", "scroll");
+            open_status = true;
+        });
+    }
+}
+function openClose_discussion(open_status) {
+    if(open_status) {
+        $(document).ready(function(){
+            $(".discussion-background").fadeTo(200, 1);
+            $(".open-discussion-window").fadeTo(200, 1);
+            $("body").css("overflow", "hidden");
+            open_status = false;
+        });
+    } if(!open_status) {
+        $(document).ready(function(){
+            $(".discussion-background").fadeOut(200);
+            $(".open-discussion-window").fadeOut(200);
             $("body").css("overflow", "scroll");
             open_status = true;
         });
@@ -26,7 +44,7 @@ function openClose_section() {
 $(document).ready(function(){
     $(".txta").focusin(function(){
         $(".txta").keyup(function(){
-            document.getElementById('symbol-count').innerHTML = 255 - this.value.length;
+            document.getElementById('symbol-count').innerHTML = 300 - this.value.length;
             if($(".txta").val().length <= 3) {
                 $("#post-button").css("filter", "grayscale(100%)");
                 $("#alert-symbol").css("visibility", "visible");
@@ -39,6 +57,34 @@ $(document).ready(function(){
     $(".txta").focusout(function(){
         $("#alert-symbol").css("visibility", "hidden");
     });
+    $(".inp").focusin(function(){
+        $(this).keyup(function(){
+            document.getElementById('symbol-count-topic').innerHTML = 50 - this.value.length;
+            if($(this).val().length <= 10) {
+                $("#alert-symbol-disc").css("visibility", "visible");
+            } else {
+                $("#alert-symbol-disc").css("visibility", "hidden");
+            }
+
+        });
+    });
+    $(".inp").focusout(function(){
+        $("#alert-symbol-disc").css("visibility", "hidden");
+    });
+    $(".txta2").focusin(function(){
+        $(this).keyup(function(){
+            document.getElementById('symbol-count-text').innerHTML = 600 - this.value.length;
+            if($(this).val().length <= 20) {
+                $("#alert-symbol-disc2").css("visibility", "visible");
+            } else {
+                $("#alert-symbol-disc2").css("visibility", "hidden");
+            }
+        });
+    });
+    $(".txta2").focusout(function(){
+        $("#alert-symbol-disc2").css("visibility", "hidden");
+    });
+
 });
 
 // Hides and unhides thread's text window
