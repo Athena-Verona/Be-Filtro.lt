bee9418d00036612f4bec6aa993299b349457765
Roooooooooooookas <rokas.kartenis@gmail.com>
2023-01-15 21:05:35 +0200

    Parties page (update)
    Oops

--- src/parties.js
+++ src/parties.js
@@ -1,6 +1,59 @@
+// Firebase ▼-▼
+
+// Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs, getDoc, query, where, onSnapshot } from "firebase/firestore";
+
+// Import Firebase config
+import firebaseConfig from "./utils/firebase-config"
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize services
+const db = getFirestore();
+
+// Collection reference
+
+const querySnapshot = await getDocs(collection(db, "partijos"));
+let titles = [], imgs = [], nicks = [];
+const img_titles = [];
+
+querySnapshot.forEach((doc) => {
+    titles.push(doc.data().pavadinimas);
+    imgs.push("../assets/" + doc.data().pavadinimas + ".png");
+    nicks.push(doc.data().nick);
+});
+
+for(let i = 0; i < titles.length; i++) {
+    img_titles.push({title: titles[i], image: imgs[i], nick: nicks[i]});
+}
+
+function template(card) {
+    return `
+        <div id="party-info">
+            <div class="top-text">
+                <div class="img"><img src="${card.image}"></div>
+                <div class="h"><h1> ${card.title} </h1></div>
+            </div>
+            <hr>
+            <p> ${card.nick} </p>
+        </div>
+    `
+}
+const map = img_titles.map(card => template(card));
+const html = map.join(' ');
+document.querySelector('.parties').innerHTML = html;
+
+// Firebase ▲-▲
+// Main ▼-▼
+
 // Import essential functions
 import pageEssentials from "./utils/navbar";
 import './styles/parties.css';
 
 // Initialize essential functions
 pageEssentials()
+
+// Main ▲-▲
